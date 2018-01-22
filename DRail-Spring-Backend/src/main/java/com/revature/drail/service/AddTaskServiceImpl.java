37df26c971b1ce890af8192f5cec6dcd916cb560
Charlie Harris <harrischarliep@gmail.com>
2018-01-22 00:11:13 -0500

    Implemented features: as team member, can create/update a task

---
+++ DRail-Spring-Backend/src/main/java/com/revature/drail/service/AddTaskServiceImpl.java
@@ -0,0 +1,29 @@
+package com.revature.drail.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.drail.beans.DrailTask;
+import com.revature.drail.beans.DrailTile;
+import com.revature.drail.dto.DrailTaskDTO;
+import com.revature.drail.repo.DrailTaskRepo;
+
+@Service
+public class AddTaskServiceImpl implements AddTaskService {
+	
+	@Autowired
+	private DrailTaskRepo taskRepo;
+	
+	public void saveTask(DrailTaskDTO taskDTO) {
+		DrailTask task = new DrailTask();
+		task.setName(taskDTO.getName());
+		task.setCompleted(taskDTO.isCompleted() ? 0 : 1);
+		task.setOrder(taskDTO.getOrder());
+		DrailTile tile = new DrailTile();
+		tile.setTileId(taskDTO.getTileId());
+		task.setTile(tile);
+		taskRepo.save(task);
+	}
+
+
+}
