fa0c14cd2e7f73d1c5d0fed07a9ffe8aa2314fb7
Charlie Harris <harrischarliep@gmail.com>
2018-01-22 10:38:36 -0500

    Changed DrailUser, DrailStation beans to hold maps instead of sets, no longer need URS bean to deal with role separately

--- DRail-Spring-Backend/src/main/java/com/revature/drail/dto/DrailUserDTO.java
+++ DRail-Spring-Backend/src/main/java/com/revature/drail/dto/DrailUserDTO.java
@@ -1,6 +1,11 @@
 package com.revature.drail.dto;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import com.revature.drail.beans.DrailStation;
 import com.revature.drail.beans.DrailUser;
+import com.revature.drail.beans.DrailUserRole;
 
 public class DrailUserDTO{
 	
@@ -10,6 +15,7 @@ public class DrailUserDTO{
 	private String firstname;
 	private String lastname;
 	private String email;
+	private Map<Integer, DrailUserRole> stationRoleMap = new HashMap<>();
 	
 	public DrailUserDTO() {
 	}
@@ -30,6 +36,24 @@ public class DrailUserDTO{
 		this.firstname = user.getFirstname();
 		this.lastname = user.getLastname();
 		this.email = user.getEmail();
+		if (user.getStationRoleMap() != null) {
+			for(DrailStation station : user.getStationRoleMap().keySet()) {
+				DrailUserRole role = user.getStationRoleMap().get(station);
+				this.stationRoleMap.put(station.getStationId(), user.getStationRoleMap().get(station));
+			}
+		}
+	}
+
+	public DrailUserDTO(int userId, String username, String password, String firstname, String lastname, String email,
+			Map<Integer, DrailUserRole> stationRoleMap) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.email = email;
+		this.stationRoleMap = stationRoleMap;
 	}
 
 	public int getUserId() {
@@ -80,10 +104,19 @@ public class DrailUserDTO{
 		this.email = email;
 	}
 
+	public Map<Integer, DrailUserRole> getStationRoleMap() {
+		return stationRoleMap;
+	}
+
+	public void setStationRoleMap(Map<Integer, DrailUserRole> stationRoleMap) {
+		this.stationRoleMap = stationRoleMap;
+	}
+
 	@Override
 	public String toString() {
 		return "DrailUserDTO [userId=" + userId + ", username=" + username + ", password=" + password + ", firstname="
-				+ firstname + ", lastname=" + lastname + ", email=" + email + "]";
+				+ firstname + ", lastname=" + lastname + ", email=" + email + ", stationRoleMap=" + stationRoleMap
+				+ "]";
 	}
-	
+
 }
