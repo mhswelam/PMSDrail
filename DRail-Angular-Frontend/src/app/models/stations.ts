902d18e5e5971c14d887b521317ed6f84904300f
Allan <allanjpoindexter@outlook.com>
2018-01-24 16:43:19 -0500

    Removes getters and Setters

--- DRail-Angular-Frontend/src/app/models/stations.ts
+++ DRail-Angular-Frontend/src/app/models/stations.ts
@@ -1,19 +1,10 @@
 import { Station } from './station';
 
 export class Stations {
-    private _stations: Station[];
+    public stations: Station[];
 
     constructor(stations: Station[]) {
-        this._stations = stations;
-    }
-
-    public get stations(): Station[] {
-        return this._stations;
-    }
-
-    public set stations(value: Station[]) {
-        if (value == null) { return; }
-        this._stations = value;
+        this.stations = stations;
     }
 }
 
--- DRail-Angular-Frontend/src/app/models/task.ts
+++ DRail-Angular-Frontend/src/app/models/task.ts
@@ -1,61 +1,15 @@
 export class Task {
-    private _taskId: number;
-    private _name: string;
-    private _completed: boolean;
-    private _order: number;
-    private _tildId: number;
+    public taskId: number;
+    public name: string;
+    public completed: boolean;
+    public order: number;
+    public tildId: number;
 
     constructor(taskId: number, name: string, completed: boolean, order: number, tileId) {
-        this._taskId = taskId;
-        this._name = name;
-        this._completed = completed;
-        this._order = order;
-        this._tildId = tileId;
-    }
-
-
-    public get taskId(): number {
-        return this._taskId;
-    }
-
-    public set taskId(value: number) {
-        if (value == null) { return; }
-        this._taskId = value;
-    }
-
-    public get name(): string {
-        return this._name;
-    }
-
-    public set name(value: string) {
-        if (value == null || value === '') { return; }
-        this._name = value;
-    }
-
-    public get completed(): boolean {
-        return this._completed;
-    }
-
-    public set completed(value: boolean) {
-        if (value == null) { return; }
-        this._completed = value;
-    }
-
-    public get order(): number {
-        return this._order;
-    }
-
-    public set order(value: number) {
-        if (value == null) { return; }
-        this._order = value;
-    }
-
-    public get tildId(): number {
-        return this._tildId;
-    }
-
-    public set tildId(value: number) {
-        if (value == null) { return; }
-        this._tildId = value;
+        this.taskId = taskId;
+        this.name = name;
+        this.completed = completed;
+        this.order = order;
+        this.tildId = tileId;
     }
 }
