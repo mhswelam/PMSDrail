6befcf71749d64046b8f57b437f8359dd2f4d825
Cristian hermida <cristian.hermida@yahoo.com>
2018-01-28 19:33:56 -0500

    reload page after create station

--- DRail-Angular-Frontend/src/app/components/stations/stations.component.ts
+++ DRail-Angular-Frontend/src/app/components/stations/stations.component.ts
@@ -5,6 +5,7 @@ import { Input } from '@angular/core/src/metadata/directives';
 import { Stations } from '../../models/stations';
 import { UserService } from '../../services/user.service';
 import { User } from '../../models/user';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-stations',
@@ -13,9 +14,16 @@ import { User } from '../../models/user';
 })
 export class StationsComponent implements OnInit {
 
+  public static refreshStations: Subject<boolean> = new Subject();
   user: User;
   stations: Station[];
-  constructor(private getStationsService: GetStationsService, private userService: UserService) { }
+  constructor(private getStationsService: GetStationsService, private userService: UserService) {
+    StationsComponent.refreshStations.subscribe(
+      res => {
+        this.getStationInfo();
+        this.getUserInfo();
+      });
+   }
 
   ngOnInit() {
     this.getStationInfo();
