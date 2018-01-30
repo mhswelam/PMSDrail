c3d8c69c81d178f628e34dd0553e3c35f74bea4e
mmalhind <mmalhind@umich.edu>
2018-01-30 10:50:23 -0500

    NOW DEPLOYABLE

--- DRail-Spring-Backend/src/test/java/com/revature/drail/DrailApplicationTests.java
+++ DRail-Spring-Backend/src/test/java/com/revature/drail/DrailApplicationTests.java
@@ -1,16 +1,16 @@
-package com.revature.drail;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-public class DrailApplicationTests {
-
-	@Test
-	public void contextLoads() {
-	}
-
-}
+//package com.revature.drail;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class DrailApplicationTests {
+//
+//	@Test
+//	public void contextLoads() {
+//	}
+//
+//}
